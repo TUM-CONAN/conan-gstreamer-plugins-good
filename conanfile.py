ace49235b93e66ea60d5744f0d5e9cf4b5ebb6fc
Ulrich Eck <ulrich.eck@tum.de>
2020-04-26 11:21:16 +0200

    compile on camp conan env without opengl/x11 dependencies

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,14 @@ from conans import ConanFile, Meson, tools
 
 class GStreamerPluginsGoodConan(ConanFile):
     name = "gstreamer-plugins-good"
+    version = "1.16.2"
     description = "Plug-ins is a set of plugins that we consider to have good quality code and correct functionality"
     license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "autodetect": [True, False],
+        "gl": [True, False],
+        "x11": [True, False],
         "rtp": [True, False],
         "rtsp": [True, False],
         "udp": [True, False],
@@ -27,41 +30,45 @@ class GStreamerPluginsGoodConan(ConanFile):
     }
     default_options = (
         "autodetect=True",
+        "gl=False",
+        "x11=False",
         "rtp=True",
         "rtsp=True",
         "udp=True",
-        "png=True",
+        "png=False",
         "isomp4=True",
         "videofilter=True",
-        "vpx=True",
+        "vpx=False",
         "multifile=True",
         "matroska=True",
         "videomixer=True",
-        "ximagesrc=True",
+        "ximagesrc=False",
         "ximagesrc_xdamage=False",
-        "ximagesrc_xshm=True",
-        "jpeg=True"
+        "ximagesrc_xshm=False",
+        "jpeg=False"
     )
+    generators = "pkgconf"
 
-    def set_version(self):
-        git = tools.Git(folder=self.recipe_folder)
-        tag, branch = git.get_tag(), git.get_branch()
-        self.version = tag if tag and branch.startswith("HEAD") else branch
+    # def set_version(self):
+    #     git = tools.Git(folder=self.recipe_folder)
+    #     tag, branch = git.get_tag(), git.get_branch()
+    #     self.version = tag if tag and branch.startswith("HEAD") else branch
 
     def build_requirements(self):
-        self.build_requires("generators/[>=1.0.0]@%s/stable" % self.user)
-        self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
+        self.build_requires("generators/[>=1.0.0]@camposs/stable")
+        self.build_requires("meson/[>=0.51.2]@camposs/stable")
 
     def requirements(self):
-        self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
+        self.requires("glib/[>=2.62.0]@camposs/stable")
         gst_version = "master" if self.version == "master" else "[~%s]" % self.version
         gst_channel = "testing" if self.version == "master" else "stable"
         self.requires("gstreamer-plugins-base/%s@%s/%s" % (gst_version, self.user, gst_channel))
-        self.requires("libpng/[>=1.6.37]@%s/stable" % self.user)
-        if self.options.vpx:
-            self.requires("libvpx/[>=1.8.0]@%s/stable" % self.user)
-        if self.options.jpeg:
-            self.requires("libjpeg-turbo/[>=2.0.3]@%s/stable" % self.user)
+        # @todo: not yet wrapped
+        # self.requires("libpng/[>=1.6.37]@camposs/stable")
+        # if self.options.vpx:
+        #     self.requires("libvpx/[>=1.8.0]@camposs/stable")
+        # if self.options.jpeg:
+        #     self.requires("libjpeg-turbo/[>=2.0.3]@camposs/stable")
 
     def source(self):
         git = tools.Git(folder="gst-plugins-good-" + self.version)
@@ -71,6 +78,8 @@ class GStreamerPluginsGoodConan(ConanFile):
     def build(self):
         args = ["--auto-features=disabled"]
         args.append("-Dautodetect=" + ("enabled" if self.options.autodetect else "disabled"))
+        args.append("-Dgl=" + ("enabled" if self.options.gl else "disabled"))
+        args.append("-Dx11=" + ("enabled" if self.options.x11 else "disabled"))
         args.append("-Drtp=" + ("enabled" if self.options.rtp else "disabled"))
         args.append("-Drtsp=" + ("enabled" if self.options.rtsp else "disabled"))
         args.append("-Drtpmanager=" + ("enabled" if self.options.rtp else "disabled"))
